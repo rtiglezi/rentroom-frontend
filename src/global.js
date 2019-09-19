ddfc9fc4d921ea44bf04af571695791fb35a501a
Ronaldo Tonioli Iglezias <ronaldotonioli@gmail.com>
2019-09-19 18:29:19 -0300

    salasol start

--- src/global.js
+++ src/global.js
@@ -1,7 +1,8 @@
 import Vue from "vue";
 
 export const userKey = "__ceproc_user";
-export const baseApiUrl = "http://localhost:3000"
+export const baseApiUrl = "https://salasol.herokuapp.com";
+//export const baseApiUrl = "http://localhost:3000";
 //export const baseApiUrl = "http://192.168.15.27:3000";
 //export const baseApiUrl = "http://192.168.43.101:3000";
 //export const baseApiUrl = "http://salasol-com-br.umbler.net";
