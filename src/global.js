94cc6d0e0c5ee77545493f16c9396ef99a051505
Ronaldo Tonioli Iglezias <ronaldotonioli@gmail.com>
2019-09-19 18:20:41 -0300

    primeiro push no heroku

--- src/global.js
+++ src/global.js
@@ -1,9 +1,10 @@
 import Vue from "vue";
 
 export const userKey = "__ceproc_user";
-export const baseApiUrl = "http://192.168.15.27:3000";
+export const baseApiUrl = "http://localhost:3000"
+//export const baseApiUrl = "http://192.168.15.27:3000";
 //export const baseApiUrl = "http://192.168.43.101:3000";
-//export const baseApiUrl = "http://localhost:3000";
+//export const baseApiUrl = "http://salasol-com-br.umbler.net";
 
 export function showError(e) {
   Vue.toasted.global.defaultError({ msg: e });
